19312cb4de736cf50d17d40e56cb2479c879344f
Kevin Bowrin <kevin_bowrin@carleton.ca>
2018-02-28 12:40:58 -0500

    Complete rewrite - process then access

--- SierraWebOPAC.lua
+++ SierraWebOPAC.lua
@@ -16,30 +16,30 @@ local browser = nil
 function Init()
 	if GetFieldValue("Item", "ItemType") == "MON" then
 		interfaceMngr = GetInterfaceManager()
-				
+
 		-- Create a form
 		form = interfaceMngr:CreateForm("Sierra", "Script")
-		
+
 		-- Add a browser
 		browser = form:CreateBrowser("Sierra", "Sierra", "Sierra")
-		
+
 		-- Hide the text label
 		browser.TextVisible = false
-		
+
 		-- Since we didn't create a ribbon explicitly before creating our browser, it will have created one using the name we passed the CreateBrowser method.  We can retrieve that one and add our buttons to it.
 		ribbonPage = form:GetRibbonPage("Sierra")
-		
+
 		-- Create the search and import buttons.  We are storing the import button so we can enable it and disable it as needed.
 		ribbonPage:CreateButton("Search", GetClientImage("Search32"), "Search", "Sierra")
 		ribbonPage:CreateButton("Import Data", GetClientImage("ImportData32"), "ImportData", "Sierra")
-		
+
 		-- After we add all of our buttons and form elements, we can show the form.
 		form:LoadLayout("SierraWebOPACLayout.xml")
 		form:Show()
 
 		if autoSearch then
-			  Search()
-   		end
+			Search()
+		end
 	end
 end
 
@@ -67,112 +67,119 @@ function ImportData()
 	if maincontent == nil then
 		return
 	end
-	
+
 	local pre = maincontent:GetElementsByTagName("pre")
 	if pre == nil then
 		return
 	end
-	
-	local marc = pre:get_Item(0)
-	if marc == nil then
+
+	local unprocessedMarc = pre:get_Item(0)
+	if unprocessedMarc == nil then
 		return
 	end
 
-	marc = marc.InnerText
+	unprocessedMarc = unprocessedMarc.InnerText
 
-	local combinedTitle = ""
-	local processingTitle = false
-	local foundISXN = false
-	local foundEditor = false
+	local marc = {}
+	local lastTag = ""
+	local lastSubtag = ""
 
-	for s in marc:gmatch("[^\r\n]+") do
-
-		-- Title
-		local firstLine = false
-		local title = s:match("^245 .. (.+)$")
-		if (title ~= nil and processingTitle == false) then
-			combinedTitle = title
-			processingTitle = true
-			firstLine = true
+	for line in unprocessedMarc:gmatch("[^\r\n]+") do
+		local tag, subtag, value = line:match("^(...).(..).(.*)$")
+		tag = trim(tag)
+		subtag = trim(subtag)
+		value = trim(value)
+		if tag == "LEA" then
+		elseif tag == "" then
+			lastAddedIndex = #marc[lastTag][lastSubtag]
+			marc[lastTag][lastSubtag][lastAddedIndex] = marc[lastTag][lastSubtag][lastAddedIndex] .. " " .. value
+		else
+			if marc[tag] == nil then marc[tag] = {} end
+			if marc[tag][subtag] == nil then marc[tag][subtag] = {} end
+			table.insert(marc[tag][subtag], value)
+			lastTag = tag
+			lastSubtag = subtag
 		end
+	end
 
-		local continuedTitle = s:match("^       (.+)$")
-		if (continuedTitle ~= nil and processingTitle == true) then 
-			combinedTitle = trim(combinedTitle) .. " " .. trim(continuedTitle)
+	-- Title
+	if marc["245"] ~= nil then
+		local title = ""
+		if     marc["245"]["00"] ~= nil then title = marc["245"]["00"][1]
+		elseif marc["245"]["10"] ~= nil then title = marc["245"]["10"][1]
+		elseif marc["245"]["13"] ~= nil then title = marc["245"]["13"][1] end
+		if title ~= "" then
+			local finalTitle = title:match("^(.-) ?/ ?.*$")
+			if finalTitle == nil then
+				finalTitle = title
+			else
+				finalTitle = finalTitle .. "."
+			end
+			SetFieldValue("Item", "Title", removeSubFieldMarkers(finalTitle))
 		end
+	end
 
-		local hasField = s:find("^%d%d%d .*$")
-		if (hasField ~= nil and processingTitle == true and firstLine == false) then
-			local finalTitle = combinedTitle:match("^(.-) ?/ ?.*$")
-			finalTitle = finalTitle .. "."
-			SetFieldValue("Item", "Title", trim(removeSubFieldMarkers(finalTitle)))
-			processingTitle = false
-		end
+	-- Author
+	if (marc["100"] ~= nil and marc["100"]["1"] ~= nil) then
+		SetFieldValue("Item", "Author", removeSubFieldMarkers(marc["100"]["1"][1]))
+	end
 
-		-- Author
-		local author = s:match("^100 1  (.-)$")
-		if author ~= nil then 
-			SetFieldValue("Item", "Author", trim(removeSubFieldMarkers(author)))
-		end
+	-- Call Number
+	if (marc["090"] ~= nil and marc["090"]["1"] ~= nil) then
+		SetFieldValue("Item", "Callnumber", removeSubFieldMarkers(marc["090"]["1"][1]))
+	end
 
-		-- Call Number
-		local callNumber = s:match("^090 1  (.+)$")
-		if callNumber ~= nil then 
-			SetFieldValue("Item", "Callnumber", trim(removeSubFieldMarkers(callNumber)))
-		end
+	-- ISXN
+	if (marc["020"] ~= nil and marc["020"][""] ~= nil) then
+		isxn = marc["020"][""][1]:match("^([a-zA-Z0-9]+).*$")
+		SetFieldValue("Item", "ISXN", removeSubFieldMarkers(isxn))
+	end
 
-		-- ISXN
-		local isxn = s:match("^020    ([a-zA-Z0-9]+) .*$")
-		if (isxn ~= nil and foundISXN == false) then 
-			SetFieldValue("Item", "ISXN", trim(removeSubFieldMarkers(isxn)))
-			foundISXN = true
-		end
+	-- Edition
+	if (marc["250"] ~= nil and marc["250"][""] ~= nil) then
+		SetFieldValue("Item", "Edition", removeSubFieldMarkers(marc["250"][""][1]))
+	end
 
-		-- Edition
-		local edition = s:match("^250    (.+)$")
-		if edition ~= nil then
-			SetFieldValue("Item", "Edition", trim(removeSubFieldMarkers(edition)))
-		end
+	-- Pages
+	if (marc["300"] ~= nil and marc["300"][""] ~= nil) then
+		pages = marc["300"][""][1]:match("^.-([0-9]-)]? p\..*$")
+		SetFieldValue("Item", "PagesEntireWork", pages)
+	end
 
-		-- Pages
-		local pages = s:match("^300    .-([0-9]-)]? p\..*$")
-		if pages ~= nil then
-			SetFieldValue("Item", "PagesEntireWork", trim(pages))
-		end
+	-- Year
+	if (marc["260"] ~= nil and marc["260"][""] ~= nil) then
+		SetFieldValue("Item", "JournalYear", getYear(marc["260"][""][1]))
+	end
+	if (marc["264"] ~= nil and marc["264"][""] ~= nil) then
+		SetFieldValue("Item", "JournalYear", getYear(marc["264"][""][1]))
+	end
 
-		-- Year
-		local year260 = s:match("^260 .. .-|c([0-9][0-9][0-9][0-9])\. $")
-		if year260 ~= nil then
-			SetFieldValue("Item", "JournalYear", trim(year260))
-		end
-		local year264 = s:match("^264 .. .-|c([0-9][0-9][0-9][0-9])\. $")
-		if year264 ~= nil then
-			SetFieldValue("Item", "JournalYear", trim(year264))
-		end
-
-		-- Editor
-		local editor = s:match("^700 10 (.+)$")
-		if (editor ~= nil and foundEditor == false) then
-			SetFieldValue("Item", "Editor", trim(removeSubFieldMarkers(editor)))
-			foundEditor = true
-		end
+	-- Editor
+	if (marc["700"] ~= nil and marc["700"]["10"] ~= nil) then
+		SetFieldValue("Item", "Editor", removeSubFieldMarkers(marc["700"]["10"][1]))
 	end
 
 	ExecuteCommand("SwitchTab", {"Details"})
 end
 
 function trim(s)
-  return (s:gsub("^%s*(.-)%s*$", "%1"))
+	return (s:gsub("^%s*(.-)%s*$", "%1"))
 end
 
 function removeSubFieldMarkers(s)
-  return (s:gsub("|[a-zA-Z0-9]", " "))
+	local subsDone = s:gsub("|[a-zA-Z0-9]", " ")
+	return (subsDone:gsub("  ", " "))
 end
 
 function urlEncode(str)
-	str = string.gsub (str, "\n", "\r\n");
-	str = string.gsub (str, "([^%w ])",
-			function (c) return string.format ("%%%02X", string.byte(c)) end);
-	str = string.gsub (str, " ", "+");
-	return str;
+	str = str:gsub("\n", "\r\n")
+	str = str:gsub("([^%w ])",
+			function (c) return string.format ("%%%02X", string.byte(c)) end)
+	return (str:gsub(" ", "+"))
+end
+
+function getYear(value)
+	year = value:match("^.-|c%[?([0-9][0-9][0-9][0-9])%]?%.?$")
+	if year == nil then year = value:match("^.-%[?([0-9][0-9][0-9][0-9])%]?$") end
+	return year
 end
